9e116a0c5070f57ffdca4d6a0acca743465e4865
coderpro1102 <meghana.sancheti@gmail.com>
2025-03-15 23:03:51 +0530

    fixing errors

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,25 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli-task-manager",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "cli-taskmanager-in-Go",
+	Short: "A CLI task manager",
+	Long:  `A simple CLI task manager built with Cobra`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("CLI Task Manager")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +42,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/meghana-0211/cli-task-manager/cmd"
+import "github.com/meghana-0211/cli-taskmanager-in-Go/cmd"
 
 func main() {
 	cmd.Execute()
